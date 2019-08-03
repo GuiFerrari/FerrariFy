8abaa6fd187dcf6133f495904e4eedea1b81eab6
Guilherme Ferrari <guiferrari10@hotmail.com>
2019-08-03 18:17:07 -0300

    Add Header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 80px;
+  padding: 0 30px;
+  background: #ff4854;
+  background: linear-gradient(45deg, rgba(255,55,95,1) 0%, rgba(255,87,75,1) 100%);
+  color: #FFF;
+  display: flex;
+  align-items: center;
+`;
