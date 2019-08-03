42a449a829419e4652cdf5ecdd3d7099d1c48db5
Guilherme Ferrari <guiferrari10@hotmail.com>
2019-08-03 19:54:39 -0300

    Adding API and styles

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Container } from './styles';
+import { loadLists } from '../../services/api';
 
-// import { Container } from './styles';
+import List from '../List';
+
+const lists = loadLists();
 
 export default function Board() {
   return (
-    <div />
+    <Container>
+      {
+        lists.map(list => <List key={list.title} data={list} />)
+      }
+    </Container>
   );
 }
