a4558e5c26f192e0143c1684f0511f95b61b0829
Guilherme Ferrari <guiferrari10@hotmail.com>
2019-08-05 20:14:16 -0300

    Add multiple functions and dependence 'immer'

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from './styles';
 import { loadLists } from '../../services/api';
+import BoardContext from './context';
 
 import List from '../List';
 
-const lists = loadLists();
+const data = loadLists();
 
 export default function Board() {
+
+  const [lists, setLists] = useState(data);
+
+  function move(from, to) {
+    console.log(from, to)
+  }
+
   return (
-    <Container>
-      {
-        lists.map(list => <List key={list.title} data={list} />)
-      }
-    </Container>
+    <BoardContext.Provider value={{ lists, move }}>
+      <Container>
+        {
+          lists.map(list => <List key={list.title} data={list} />)
+        }
+      </Container>
+    </BoardContext.Provider>
   );
 }
